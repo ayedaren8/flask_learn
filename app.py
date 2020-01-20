a3718ede8d485f1309db7ff9ad950c7057b165cc
ayedaren <1783580635@qq.com>
2020-01-20 22:12:57 +0800

    wow

--- app.py
+++ app.py
@@ -1,30 +1,11 @@
 from flask import Flask,render_template
-from flask_sqlalchemy import SQLAlchemy
-import os 
 app = Flask(__name__)
-db = SQLAlchemy(app) 
 
-name = 'Grey Li'
-movies = [
-    {'title': 'My Neighbor Totoro', 'year': '1988'},
-    {'title': 'Dead Poets Society', 'year': '1989'},
-    {'title': 'A Perfect World', 'year': '1993'},
-    {'title': 'Leon', 'year': '1994'},
-    {'title': 'Mahjong', 'year': '1996'},
-    {'title': 'Swallowtail Butterfly', 'year': '1996'},
-    {'title': 'King of Comedy', 'year': '1999'},
-    {'title': 'Devils on the Doorstep', 'year': '1999'},
-    {'title': 'WALL-E', 'year': '2008'},
-    {'title': 'The Pork of Music', 'year': '2012'},
-]
 
 @app.route('/')
 def index():
-    return render_template('index.html',name=name,movies=movies)
+    return render_template('index.html')
     
 
-
-
-
 if __name__ == "__main__":
-     app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:////' + os.path.join(app.root_path, 'data.db')
+     app.run()
